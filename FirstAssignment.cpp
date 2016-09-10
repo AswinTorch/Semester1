5842d36447f188c6138b1d26ecf7d92ad59528b1
Aswin Nair <aswintorch@gmail.com>
2016-09-09 21:22:58 -0400

    Update FirstAssignment.cpp

--- FirstAssignment.cpp
+++ FirstAssignment.cpp
@@ -1,35 +1,28 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-#include <cmath>
 #include <algorithm>
 using namespace std;
+
 int main() {
     int playerMoney = 100, machineMoney = 100;
     int playerDice1, playerDice2, playerDice3, machineDice1, machineDice2, machineDice3;
     int playerBet, playerTotalScore, machineTotalScore, roundNumber = 0;
     srand(time(0));
-    cout << "Welcome to \"Going To Boston\"!\n";
-    cout << "=============================\n";
     //Loop
-    while (true){
-        playerDice1 = rand() % 6 + 1;                                                                                   //Random dice rolling
-        playerDice2 = rand() % 6 + 1;
-        playerDice3 = rand() % 6 + 1;
-        machineDice1 = rand() % 6 + 1;
-        machineDice2 = rand() % 6 + 1;
-        machineDice3 = rand() % 6 + 1;
+    while (true) {
+        playerDice1 = rand() % 6 + 1, playerDice2 = rand() % 6 + 1, playerDice3 = rand() % 6 + 1;                       //Random dice rolling
+        machineDice1 = rand() % 6 + 1, machineDice2 = rand() % 6 + 1, machineDice3 = rand() % 6 + 1;
         ++roundNumber;                                                                                                  //Changes round number
         cout << "[Round " << roundNumber << "] You rolled ( " << playerDice1 << ", " << playerDice2 << ", " << "? ) ";
         cout << "and the Machine rolled ( " << machineDice1 << ", " << machineDice2 << ", " << "? ).\n";
         cout << "How much will you bet: ";
         cin >> playerBet;
-        if (playerBet > 10){                                                                                            //If bet is more than 10, resets.
-            cout << "***Your bet must be between 1 - 10***\n";
-            cout << "How much will you bet: ";
+        while (playerBet > 10 || playerBet < 1) {                                                                       //If bet > 10 or < 1, resets.
+            cout << "Your bet must be between $1 - $10 , how much will you bet: ";
             cin >> playerBet;
         }
-        cout << "\nYou had ( " << playerDice1 << ", " << playerDice2 << ", " << playerDice3 << " ) --> ";               //Player Stuff
+        cout << "You had ( " << playerDice1 << ", " << playerDice2 << ", " << playerDice3 << " ) --> ";                 //Player Stuff
         if (playerDice1 == playerDice2 && playerDice2 == playerDice3){
             cout << "BIG FISH!";
         }
@@ -79,7 +72,7 @@ int main() {
             cout << "You scored " << playerTotalScore << ".";
         }
         else cout << "Oops, something went wrong.";
-        cout << "\nMachine had ( " << machineDice1 << ", " << machineDice2 << ", " << machineDice3 << " ) --> ";               //Machine Stuff
+        cout << "\nMachine had ( " << machineDice1 << ", " << machineDice2 << ", " << machineDice3 << " ) --> ";        //Machine Stuff
         if (machineDice1 == machineDice2 && machineDice2 == machineDice3) {
             cout << "BIG FISH!";
         }
@@ -138,31 +131,21 @@ int main() {
             machineMoney += playerBet * 50;
             playerMoney -= playerBet * 50;
         }
-        else if (playerTotalScore > machineTotalScore){
-            cout << "\nYou won! ";
-            machineMoney -= playerBet * abs(playerTotalScore - machineTotalScore);
-            playerMoney += playerBet * abs(playerTotalScore - machineTotalScore);
+        else {
+            playerMoney += playerBet * (playerTotalScore - machineTotalScore);
+            machineMoney -= playerBet * (playerTotalScore - machineTotalScore);
         }
-        else if (playerTotalScore < machineTotalScore){
-            cout << "\nYou lost! ";
-            machineMoney += playerBet * abs(playerTotalScore - machineTotalScore);
-            playerMoney -= playerBet * abs(playerTotalScore - machineTotalScore);
-        }
-        cout << "\nYour money: " << playerMoney << " || ";
-        cout << "AI money: " << machineMoney;
-        cout << "\n=============================\n";
-        if (playerMoney <= 0 || machineMoney <= 0){
+        cout << (playerTotalScore >= machineTotalScore ? "\nYou won! " : "\nYou lost! ");
+        cout << "You have $" << playerMoney << " and the machine has $" << machineMoney << ". \n=============================\n";
+        if (playerMoney  <= 0 || machineMoney <= 0){                                                                    //Get out of loop
             break;
         }
     }
-    //ENDGAME
-    if (playerMoney <= 0){
+    if (playerMoney <= 0){                                                                                              //ENDGAME
         cout << "YOU LOST THE GAME! :( \n";
-        cout << "Sorry, but you're just unlucky...";
     }
     else if (machineMoney <= 0){
         cout << "YOU WON THE GAME! :) \n";
-        cout << "You're so lucky!";
     }
     else if (playerMoney <= 0 && machineMoney <= 0){
         cout << "IT'S A TIE!";
