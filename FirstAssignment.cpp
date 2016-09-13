8c00da812a70b6227691456a72359aeedb0bc526
Aswin Nair <aswintorch@gmail.com>
2016-09-13 14:46:58 -0400

    Update FirstAssignment.cpp

--- FirstAssignment.cpp
+++ FirstAssignment.cpp
@@ -1,43 +1,54 @@
+#define AUTO_INPUT                       //Comment this line out to end simulation and allow the player to input bet.
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
 #include <algorithm>
 using namespace std;
-
+int roll() {return rand() % 6 + 1;}
 int maxFunction(int a, int b, int c, int score){
-    b = rand() % 6 + 1;
-    c = rand() % 6 + 1;
+    b = roll(), c = roll();
     cout << "You rolled ( " << b << ", " << c << " ) --> ";
     if (max(b, c) == b){
-        c = rand() % 6 + 1;
+        c = roll();
         cout << "You rolled ( " << c << " ) --> ";
     }
     else if (max(b, c) == c){
-        b = rand() % 6 + 1;
+        b = roll();
         cout << "You rolled ( " << b << " ) --> ";
     }
     score = a + b + c;
     cout << "You scored " << score << ".";
     return score;
 }
+
 int main() {
-    int playerMoney = 100, machineMoney = 100;
+    int playerMoney = 1000, machineMoney = 1000;
     int playerDice1, playerDice2, playerDice3, machineDice1, machineDice2, machineDice3;
     int playerBet, playerTotalScore, machineTotalScore, roundNumber = 0;
     srand(time(0));
     //Loop
     while (true) {
-        playerDice1 = rand() % 6 + 1, playerDice2 = rand() % 6 + 1, playerDice3 = rand() % 6 + 1;                       //Random dice rolling
-        machineDice1 = rand() % 6 + 1, machineDice2 = rand() % 6 + 1, machineDice3 = rand() % 6 + 1;
+        playerDice1 = roll(), playerDice2 = roll(), playerDice3 = roll();                                               //Random dice rolling
+        machineDice1 = roll(), machineDice2 = roll(), machineDice3 = roll();
         ++roundNumber;                                                                                                  //Changes round number
         cout << "[Round " << roundNumber << "] You rolled ( " << playerDice1 << ", " << playerDice2 << ", " << "? ) ";
         cout << "and the Machine rolled ( " << machineDice1 << ", " << machineDice2 << ", " << "? ).\n";
+#ifndef AUTO_INPUT
         cout << "How much will you bet: ";
         cin >> playerBet;
         while (playerBet > 10 || playerBet < 1) {                                                                       //If bet > 10 or < 1, resets.
             cout << "Your bet must be between $1 - $10 , how much will you bet: ";
             cin >> playerBet;
         }
+#else                                                                                                                   //Simulation
+        if (playerDice1 == playerDice2){
+            playerBet = 10;
+        }
+        else if (machineDice1 == machineDice2){
+            playerBet = 1;
+        } else playerBet = rand() % 5 + 1;
+        cout << "You have automatically bet: " << playerBet << endl;
+#endif
         cout << "You had ( " << playerDice1 << ", " << playerDice2 << ", " << playerDice3 << " ) --> ";                 //Player Stuff
         if (playerDice1 == playerDice2 && playerDice2 == playerDice3){
             cout << "BIG FISH!";
@@ -50,8 +61,7 @@ int main() {
         }
         else if (max(max(playerDice1, playerDice2), playerDice3) == playerDice3){
             playerTotalScore = maxFunction(playerDice3, playerDice2, playerDice1, playerTotalScore);                                                                      //Calls function to test max
-        }
-        else cout << "Oops, something went wrong.";
+        } else cout << "Oops, something went wrong.";
         cout << "\nMachine had ( " << machineDice1 << ", " << machineDice2 << ", " << machineDice3 << " ) --> ";        //Machine Stuff
         if (machineDice1 == machineDice2 && machineDice2 == machineDice3) {
             cout << "BIG FISH!";
@@ -65,7 +75,7 @@ int main() {
         else if (max(max(machineDice1, machineDice2), machineDice3) == machineDice3){
             machineTotalScore = maxFunction(machineDice3, machineDice2, machineDice1, machineTotalScore);
         } else cout << "Oops, something went wrong.";
-        //Win or Lose
+        //WIN OR LOSE
         if (playerDice1 == playerDice2 && playerDice2 == playerDice3){
             playerMoney += playerBet * 50;
             machineMoney -= playerBet * 50;
@@ -78,7 +88,12 @@ int main() {
             playerMoney += playerBet * (playerTotalScore - machineTotalScore);
             machineMoney -= playerBet * (playerTotalScore - machineTotalScore);
         }
-        cout << (playerTotalScore >= machineTotalScore ? "\nYou won! " : "\nYou lost! ");
+        if (playerDice1 == playerDice2 && playerDice2 == playerDice3){
+            cout << "\nYou won!";
+        }
+        else if (machineDice1 == machineDice2 && machineDice2 == machineDice3){
+            cout << "\nYou lost!";
+        } else cout << (playerTotalScore >= machineTotalScore ? "\nYou won! " : "\nYou lost! ");
         cout << "You have $" << playerMoney << " and the machine has $" << machineMoney << ". \n=============================\n";
         if (playerMoney  <= 0 || machineMoney <= 0) break;                                                              //Get out of loop
     }
