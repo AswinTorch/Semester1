ca225c390783683a77cd7a231099c0d4cbf4d059
Aswin Nair <aswintorch@gmail.com>
2016-10-07 18:08:49 -0400

    Update ThirdAssignment.cpp

--- ThirdAssignment.cpp
+++ ThirdAssignment.cpp
@@ -1,28 +1,26 @@
+//Aswin Nair
+//Animal Quiz Word Scrambler Game
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
 #include <algorithm>
 #include <vector>
 using namespace std;
-
 int main() {
     srand(time(0));                                                                                                     //Variables
     string input;
     int wordCount, strikeCount = 0, successCount = 0, missCount = 0;
     vector<string> animalNames(0);
-
     cout << "Enter at least five animal names, e.g., cat, dog, etc..." << endl;
     while (true) {
-        cout << "> ";
-        getline(cin, input);
-        bool found = false;
-        string word;
+        cout << "> "; getline(cin, input);                                                                              //First input prompting to enter at least 5 words
+        bool found = false; string word;
         if (input == "?") {
             for (int a = 0; a < animalNames.size(); ++a) {
                 cout << a + 1 << ": " << animalNames[a] << endl;                                                        //Displays animal names entered if input is '?'
             }
         } else if (input == "quit") {
-            cout << "\nBye..."; return 0;                                                                                                   //Quits game if user enters "quit"
+            cout << "\nBye..."; return 0;                                                                               //Quits game if user enters "quit"
         } else {
             for (int a = 0; a < input.length(); ++a) {
                 if (!isspace(input[a])) {
@@ -40,83 +38,78 @@ int main() {
             if (input == "" && animalNames.size() >= 5) break;
         }
     }
-    //Game Process
     for (int a = 0; a < animalNames.size(); ++a) {                                                                      //Displays animal names entered
         cout << a + 1 << ": " << animalNames[a] << endl;
     }
-
     while (true) {
         vector<string> challengeSet(0), temporaryVector(0), inputNames(0);
         string challengeString = "", word;
         bool found = false;
-        temporaryVector = animalNames;
+        temporaryVector = animalNames;                                                                                  //Keeps animalNames intact by using a temp vector to shuffle
         random_shuffle(temporaryVector.begin(), temporaryVector.end());
         wordCount = rand() % 3 + 1;
         for (int a = 0; a < wordCount; ++a) {
             challengeSet.push_back(temporaryVector[a]);
         }
-        for (int a = 0; a < challengeSet.size(); ++a) {                                                                 //Scrambling and making string
+        for (int a = 0; a < challengeSet.size(); ++a) {                                                                 //Scrambling and creating string
             challengeString += challengeSet[a];
         }
         random_shuffle(challengeString.begin(), challengeString.end());
 
         for (int a = 0; a < strikeCount; ++a) {
             while (true) {
-                int position = rand() % challengeString.length();                                                       //Does underscore challenge under conditions
+                int position = rand() % challengeString.length();                                                       //Does underscore challenge under strike conditions
                 if (challengeString[position] == '_') continue;
                 challengeString[position] = '_';
                 break;
             }
         }
-
-        if (wordCount == 1) cout << "\nWhat is " << wordCount << " animal in \"" << challengeString << "\" ? ";
+        if (wordCount == 1) cout << "\nWhat is " << wordCount << " animal in \"" << challengeString << "\" ? ";         //Question to the user
         else cout << "\nWhat are " << wordCount << " animals in \"" << challengeString << "\" ? ";
-
         getline(cin, input);
-        if (input == "quit") {                                                                                           //Quits program if quit is entered
+        if (input == "quit") {                                                                                          //Quits program if quit is entered
             cout << "\nBye..."; return 0;
-        } else if (input == "?") {                                                                                       //Displays animal names entered if input is '?'
+        } else if (input == "?") {                                                                                      //Displays animal names entered if input is '?'
             for (int a = 0; a < animalNames.size(); ++a) {
                 cout << a + 1 << ": " << animalNames[a] << endl;
             }
-        } else {                                                                                                        //Takes in input
-            if (input.length() < challengeString.length()) {
-                cout << "Your number of input is incorrect. Enter again: ";
-                input = "";
-                getline(cin, input);
+        } else {
+            while (true) {
+                inputNames.clear(); word = "";
+                for (int a = 0; a < input.length(); ++a) {
+                    if (!isspace(input[a])) {
+                        word += input[a];
+                        found = true;
+                        continue;
+                    }                                                                                                   //For-loop that parses string, removes spaces
+                    if (!found) continue;
+                    inputNames.push_back(word);
+                    word = "";
+                    found = false;
+                }
+                if (found) inputNames.push_back(word);
+                sort(inputNames.begin(), inputNames.end());                                                             //Sorts for conditions to work
+                sort(challengeSet.begin(), challengeSet.end());
+                if (inputNames.size() == challengeSet.size()) break;
+                if (inputNames.size() < challengeSet.size() || inputNames.size() > challengeSet.size()){
+                    cout << "Your number of input is incorrect. Enter again: ";                                         //Input check
+                    getline(cin, input);
+                    if (input == "quit") cout << "\nBye..."; return 0;                                                  //Quits program if quit is entered in input check
+                }
             }
-            for (int a = 0; a < input.length(); ++a) {
-                if (!isspace(input[a])) {
-                    word += input[a];
-                    found = true;
-                    continue;
-                }                                                                                                       //For loop that parses string, removes spaces
-                if (!found) continue;
-                inputNames.push_back(word);
-                word = "";
-                found = false;
-            }
-            if (found) inputNames.push_back(word);
-            sort(inputNames.begin(), inputNames.end());
-            sort(challengeSet.begin(), challengeSet.end());                                                             //Sort both arrays and compare if both are equal
         }
-
-        if (inputNames == challengeSet) {
+        if (inputNames == challengeSet) {                                                                               //When user answers correctly
             cout << "Yes!" << endl;
-            missCount = 0;
-            ++successCount;
+            missCount = 0; ++successCount;
             if (successCount >= 2) {
-                ++strikeCount;
-                successCount = 0;
+                ++strikeCount; successCount = 0;
                 cout << "Succeeded two consecutive times, challenge goes up!" << endl;
             }
-        } else if (inputNames != challengeSet) {
-            cout << "No!" << endl;
-            successCount = 0;
-            ++missCount;
+        } else if (inputNames != challengeSet) {                                                                        //When user answers incorrectly
+            cout << "Nope!" << endl;
+            successCount = 0; ++missCount;
             if (missCount >= 2) {
-                --strikeCount;
-                missCount = 0;
+                --strikeCount; missCount = 0;
                 cout << "Missed two consecutive times, challenge goes down!" << endl;
                 if (strikeCount < 0) strikeCount = 0;
             }
